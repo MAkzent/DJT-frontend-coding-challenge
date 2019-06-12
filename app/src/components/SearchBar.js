07b90a3cbc3d9cf6d7a7ee39970ca41cb970e3d1
Luis Gonzalez <luis.egvillarreal@gmail.com12>
2019-06-11 19:24:10 -0500

    Fourth Commit
    I created the functions that enable the user to input data in the search bar and also created a button that sends the data as a http request. The data is then formated in a JSON object.

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -7,16 +7,34 @@ class SearchBar extends Component {
         this.state = { term: '' }
     }
 
+
     onInputChange(term) {
        this.setState({term});
-       this.props.onTermChange(term);
+       console.log({term})
    }
+
+   fetchurl(term){
+     fetch(term)
+     .then(response => response.json())
+     .then(data => {
+       console.log(data)
+     })
+     .catch(error => console.error(error))
+   }
+
 
   render() {
     return(
+      <div className="Containter">
       <input className="SearchBar" type="text" placeholder="Paste a link to a Github repo!" onChange={event => this.onInputChange(event.target.value)}></input>
+    <button className="Submit" onClick={event => this.fetchurl(this.state.term)}>Search</button>
+    </div>
     );
   }
 }
+
+//'https://api.github.com'
+
+
 
 export default SearchBar
