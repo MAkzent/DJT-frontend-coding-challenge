c56779f44d7dd3f779bc131381bb41befe7a13bf
Kanitkorn S <k.sujautra@gmail.com>
2019-12-18 16:08:36 +0900

    Add stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-prettier'
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: []
