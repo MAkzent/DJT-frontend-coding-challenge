f4412af25a41486184dadc6d45df2e072e0c057f
Derek <derek.forbes77@gmail.com>
2020-01-09 21:22:54 +0900

    have the body of issues being shortened if too long now

--- github-issue-getter/src/components/resultsPage/IssueFilter.jsx
+++ github-issue-getter/src/components/resultsPage/IssueFilter.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./styles/issueType.scss";
+import "./styles/issueFilter.scss";
 class IssueFilter extends Component {
   constructor(props) {
     super(props);
