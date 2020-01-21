0dbcda72eb50cbefab026926736858444bc8760f
Derek <derek.forbes77@gmail.com>
2020-01-21 15:42:44 +0900

    small font-size change on issue labels

--- github-issue-getter/src/components/resultsPage/IssueLabel.jsx
+++ github-issue-getter/src/components/resultsPage/IssueLabel.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./styles/issueLabel.scss";
 const IssueLabel = props => {
-  return <div className="label">•{props.label.name}</div>;
+  return (
+    <div className="label">
+      <div className="labelText">•{props.label.name}</div>
+    </div>
+  );
 };
 
 export default IssueLabel;
