cd899c6d64f39334dcae575ddd8e40e55b19cd58
ntaylor6422 <ntaylor6422@gmail.com>
2021-05-02 09:51:15 +0900

    Added template for hom screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="mainTitle">GitHub Issue Viewer</h1>
+    <Search />
   </div>
 </template>
 
 <script>
-export default {}
+import Search from "../components/Search";
+export default {
+  components: {
+    Search
+  }
+};
 </script>
 
 <style>
@@ -39,35 +22,13 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
+  background-color: var(--bg-main);
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.mainTitle {
+  font-size: var(--big-text);
+  color: white;
+  padding-bottom: 20px;
 }
 </style>
