1f4844aa92c79c93ebbe781435af6fdbf49fc081
ntaylor6422 <ntaylor6422@gmail.com>
2021-05-02 09:44:53 +0900

    Added fonts and css color variables

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,47 @@
 </template>
 
 <style>
+@font-face {
+  font-family: "MontReg";
+  src: url("../_assets/fonts/montserrat-regular.woff"),
+    url("../_assets/fonts/montserrat-regular.woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: "MontBold";
+  src: url("../_assets/fonts/montserrat-bold.woff"),
+    url("../_assets/fonts/montserrat-bold.woff2");
+  font-weight: bold;
+  font-style: normal;
+}
+@font-face {
+  font-family: "MontLight";
+  src: url("../_assets/fonts/montserrat-light.woff"),
+    url("../_assets/fonts/montserrat-light.woff2");
+  font-weight: lighter;
+  font-style: normal;
+}
+
+:root {
+  --bg-main: #e91e63;
+  --card-header: #3f51b5;
+  --dark-blue: #1a237e;
+  --dark-red: #880e4f;
+  --text-default: #607d8b;
+  --text-light: #cfd8dc;
+  --dark-grey: #455a64;
+
+  --big-text: 48px;
+  --head-text: 28px;
+  --title-text: 18px;
+  --para-text: 12px;
+  --mini-text: 9px;
+}
+
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "MontReg", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
